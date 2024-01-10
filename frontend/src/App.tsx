f7c20c0233201ebe7d97a29641580e0b191763f7
Yaseen Ahmed <yaseenninja3@gmail.com>
2024-01-11 02:24:12 +0530

    feat: added loading state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,50 +9,65 @@ type data = Array<{
 function App() {
   const [url, setUrl] = useState('')
   const [data, setData] = useState<data>()
+  const [loading, setLoading] = useState(false)
 
-  const extract = async () => {
-    const resp = await fetch('https://hilarious-trench-coat-clam.cyclic.app/api/sitemap?' + new URLSearchParams({
-      url
-    }))
-    const resp_body = await resp.json()
-    if(resp_body.data) {
-      setData(resp_body.data)
-    } else {
-      setData(resp_body)
-    }
+  const extract = () => {
+    setLoading(true)
+    fetch('https://hilarious-trench-coat-clam.cyclic.app/api/sitemap?' + new URLSearchParams({ url }))
+      .then((response) => response.json())
+      .then((resp_body) => {
+        if (resp_body.data) {
+          setData(resp_body.data)
+        } else {
+          setData(resp_body)
+        }
+        setLoading(false)
+      })
+      .catch((err) => {
+        console.error(err)
+        setLoading(false)
+      })    
   }
 
   return (
     <>
       <div>
-        <h1 className='pb-9'>Sitemap Extractor</h1>
-        <input type="text" placeholder="Enter URL" className="input input-bordered w-full max-w-xs mb-9" onInput={(e) => setUrl(e.currentTarget.value)}/>
-        <button className="btn mb-9" onClick={extract}>Extract</button>
-        <div className="overflow-x-auto">
-          <table className="table">
-            <thead>
-              <tr>
-                <th></th>
-                <th>URL</th>
-                <th>title</th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                data?.map((d, i) => {
-                  return(
-                  <tr key={i}>
-                    <th>{i+1}</th>
-                    <td>{d.link}</td>
-                    <td>{d.title}</td>
-                  </tr>
-                  )    
-                })
-              }
-            </tbody>
-          </table>
-        </div>
+        <h1 className='pb-9 text-3xl'>Sitemap Extractor</h1>
+        <input type="text" placeholder="Enter URL" className="input input-bordered max-w-xs" onInput={(e) => setUrl(e.currentTarget.value)} />
+        <button className="btn ml-1" disabled={loading} onClick={extract}>Extract</button>
       </div>
+      <br />
+      {
+        loading ? <div className="loading loading-spinner" /> : null
+      }
+      {
+        data ?
+          <div className="overflow-x-auto">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>URL</th>
+                  <th>Title</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  data?.map((d, i) => {
+                    return (
+                      <tr key={i}>
+                        <th>{i + 1}</th>
+                        <td>{d.link}</td>
+                        <td>{d.title}</td>
+                      </tr>
+                    )
+                  })
+                }
+              </tbody>
+            </table>
+          </div>
+          : null
+      }
     </>
   )
 }
